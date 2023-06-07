bce5d8c1854c8919336cf21542922f9c3a944e64
victorlopescc <victorlopes.cc@gmail.com>
2023-06-07 08:32:44 -0300

    v2.1 - Search and Details Style

--- js/details.js
+++ js/details.js
@@ -2,14 +2,38 @@ function getDetails(id) {
     fetch(`https://fakestoreapi.com/products/${id}`)
         .then(res => res.json())
         .then(json => {
+            let stars = [];
+            for (let i = 0; i < 5; i++) {
+                if (i < json.rating.rate) {
+                    stars.push(`<i class="bi bi-star-fill text-success"></i>`);
+                } else {
+                    stars.push(`<i class="bi bi-star text-success"></i>`);
+                }
+            }
+
             let html = `
-                <p>${json.title}</p>
+                <div id="mobile" class="row">
+                <div class="col-6">
+                    <div id="stars" class="d-flex justify-content-center fs-2">
+                        ${stars.join('')}
+                    </div>
+                    <div class="row">
+                        <img src="${json.image}" class="detailsImg">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <h3 class="text-success">R$ ${json.price}</h3>
+                    <h1>${json.title}</h1>
+                    <h6>${json.category.toUpperCase()}</h6>
+                    <p>${json.description}</p>
+                    <div class="d-flex justify-content-center">
+                        <button class="btn btn-outline-success"><i class="bi bi-bag-plus"></i></button>
+                    </div>
+                </div>
+                </div>
             `;
 
-            console.log(document);
             document.getElementById('details').innerHTML = html;
-
-
         });
 }
 
--- js/search.js
+++ js/search.js
@@ -6,7 +6,24 @@ function searchProducts(input) {
             for (let i = 0; i < json.length; i++) {
                 if (json[i].title.toLowerCase().includes(input.toLowerCase())) {
                     html += `
-                    <a href="./details.html?id=${json[i].id}">${json[i].title}</a>
+                    <a href="./details.html?id=${json[i].id}">
+                        <div class="card mb-3"">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="${json[i].image}" class="img-fluid rounded-start">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h6 class="text-success">R$ ${json[i].price}</h6>
+                                        <h5 class="card-title">${json[i].title}</h5>
+                                        <p class="card-text">${json[i].description}</p>
+                                        <p class="card-text"><small class="text-body-secondary">${json[i].category}</small>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
                     `;
                 }
             }
